e900ffeceecb2bfacbaad5c024dfa63afd7e1657
Nirmal Kumar Bhagatkar <nkbhagatkar@gmail.com>
2026-02-03 15:32:07 +0530

    updates for railway

--- HRMS_lite/settings.py
+++ HRMS_lite/settings.py
@@ -21,49 +21,16 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+# Railway dynamic host support
+if config('RAILWAY_PUBLIC_DOMAIN', default=None):
+    ALLOWED_HOSTS.append(config('RAILWAY_PUBLIC_DOMAIN'))
+
+# Automatically allow Railway domains
+ALLOWED_HOSTS.extend(['.railway.app', '.up.railway.app'])
+
 
 # Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'HRMS',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files in production
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'HRMS_lite.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'HRMS_lite.wsgi.application'
-
+# ... (rest of standard settings)
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -71,11 +38,11 @@ WSGI_APPLICATION = 'HRMS_lite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='HRMS_lite'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME': config('MYSQLDATABASE', default=config('DB_NAME', default='HRMS_lite')),
+        'USER': config('MYSQLUSER', default=config('DB_USER', default='root')),
+        'PASSWORD': config('MYSQLPASSWORD', default=config('DB_PASSWORD', default='')),
+        'HOST': config('MYSQLHOST', default=config('DB_HOST', default='localhost')),
+        'PORT': config('MYSQLPORT', default=config('DB_PORT', default='3306')),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
